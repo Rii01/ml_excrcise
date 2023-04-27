5a22d87a074ea59e0f05a273d421257bf8b0a38c
e215749 <e215749@ie.u-ryukyu.ac.jp>
2023-04-28 08:57:50 +0900

    dataset1

--- ex1/dataset1.py
+++ ex1/dataset1.py
@@ -1,20 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.array([0, -1, 0.1, 1])
 def true_function(X):
     y = np.sin(np.pi * X * 0.8) * 10
     return y
-print(true_function(x[0]))
+
 height = np.array([])
 
-for i in range(len(x)):
-    height = np.append(height, true_function(x[i]))
-print(height)
-size = np.sort(x)
-print(size)
 
-
-plt.plot(size, height)
+x = np.arange(-1, 1, 0.1)
+y = true_function(x)
+plt.plot(x, y)
 plt.show()
-plt.xlim(-1, 1)
+fig = plt.figure()
+fig.savefig('ex1_1.png')
